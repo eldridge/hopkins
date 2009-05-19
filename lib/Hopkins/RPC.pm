5cfa69cecd3d84e56358564697d8b374353ce805
diz <diz@pulse.magazines.com>
2009-05-19 16:03:44 -0500

    commit my mistaken modifications to Hopkins::RPC before i blow it away (was replaced by Hopkins::Plugin::RPC quite some time ago)

--- lib/Hopkins/RPC.pm
+++ lib/Hopkins/RPC.pm
@@ -17,13 +17,16 @@ to the job server.
 
 use POE;
 use POE::API::Peek;
+use Class::Accessor::Fast;
+
+use base 'Class::Accessor::Fast';
+
+__PACKAGE__->mk_accessors(qw(soap config));
 
 use constant HOPKINS_QUEUE_STATUS_WAIT_TIME		=> 1;
 use constant HOPKINS_QUEUE_STATUS_WAIT_ITER_MAX	=> 5;
 
-new POE::Component::Server::SOAP ALIAS => 'soap', ADDRESS => 0, PORT => 8080;
-
-my @methods =
+my @procedures =
 qw/
 	enqueue
 	status
@@ -41,17 +44,28 @@ qw/
 
 =cut
 
-sub init
+sub new
 {
+	my $self = shift->SUPER::new(@_);
+
+	$self->config({}) if not ref $self->config eq 'HASH';
+
+	$self->config->{address}	||= 0;
+	$self->config->{port}		||= 8080;
+
 	# create RPC session
 	POE::Session->create
 	(
-		inline_states => {
-			_start	=> \&start,
-			_stop	=> \&stop,
+		object_states =>
+		[
+			$self =>
+			{
+				_start	=> 'start',
+				_stop	=> 'stop',
 
-			map { $_ => \&$_ } @methods
-		}
+				map { $_ => $_ } @procedures
+			}
+		]
 	);
 }
 
@@ -61,11 +75,20 @@ sub init
 
 sub start
 {
-	my $kernel = $_[KERNEL];
+	my $self	= $_[OBJECT];
+	my $kernel	= $_[KERNEL];
+
+	my $args =
+	{
+		ALIAS	=> 'soap',
+		ADDRESS	=> $self->config->{address},
+		PORT	=> $self->config->{port}
+	};
+
+	$self->soap(new POE::Component::Server::SOAP %$args);
 
 	$kernel->alias_set('rpc');
-
-	$kernel->post(soap => ADDMETHOD => rpc => $_) foreach @methods;
+	$kernel->post(soap => ADDMETHOD => rpc => $_) foreach @procedures;
 }
 
 =item stop
@@ -76,7 +99,7 @@ sub stop
 {
 	my $kernel = $_[KERNEL];
 
-	$kernel->post(soap => DELMETHOD => rpc => $_) foreach @methods;
+	$kernel->post(soap => DELMETHOD => rpc => $_) foreach @procedures;
 }
 
 =item enqueue
@@ -133,18 +156,36 @@ sub status
 {
 	my $kernel	= $_[KERNEL];
 	my $res		= $_[ARG0];
-	my $api		= new POE::API::Peek;
+	#my $api		= new POE::API::Peek;
 
-	my @sessions = map { $kernel->alias($_) } $api->session_list;
+	#my @sessions = map { $kernel->alias($_) } $api->session_list;
 
-	my $now		= DateTime->now;
-	my $schema	= Hopkins::Store->schema;
-	my $rsTask	= $schema->resultset('Task');
+	#my $now		= DateTime->now;
+	#my $schema	= Hopkins::Store->schema;
+	#my $rsTask	= $schema->resultset('Task');
 
-	$rsTask->search({ date_completed => undef });
+	#$rsTask->search({ date_completed => undef });
 
+	#$res->content({ sessions => \@sessions });
 
-	$res->content({ sessions => \@sessions });
+	my @queues = $kernel->call(manager => 'queue_check_all');
+
+	my $status =
+	{
+		queues =>
+		{
+			map {
+				$_->name => 
+				{
+					concurrency	=> $queue->concurrency,
+					tasks		=> $queue->tasks->count,
+					status		=> 'running',
+				}
+			} @queues
+		}
+	};
+
+	$res->content($status);
 
 	$kernel->post(soap => DONE => $res);
 }
@@ -163,25 +204,42 @@ sub queue_start
 
 	Hopkins->log_debug("queue_start request received from $client for $name");
 
-	my $spawned = Hopkins::Queue->start($name);
+	my $res = $kernel->call(manager => queue_start => $name);
 
-	if ($spawned) {
-		# Hopkins::Queue spawned the Component::JobQueue.
-		# the only thing to do now is wait until that
-		# session shows itself.
+	for ($res) {
+		$_ == HOPKINS_QUEUE_STARTED && do
+		{
+			# the manager session successfully created the
+			# queue.  the only thing to do now is wait until
+			# that session shows itself.
 
-		$kernel->alarm(queue_start_waitchk => time + HOPKINS_QUEUE_STATUS_WAIT_TIME, $res, $name, 0);
-	} else {
-		# the only reason Queue->start should return false
-		# is if the specified queue wasn't found in the
-		# configuration.
-		#
-		# post a DONE event to the soap session; this will
-		# cause a SOAP response to be sent back to the
-		# client.
+			$kernel->alarm(queue_start_waitchk => time + HOPKINS_QUEUE_STATUS_WAIT_TIME, $res, $name, 0);
+			last;
+		};
 
-		$res->content({ success => 0, err => "invalid queue $name" });
-		$kernel->post(soap => DONE => $res);
+		$_ == HOPKINS_QUEUE_ALREADY_RUNNING && do
+		{
+			# the queue was already running.  post a DONE
+			# event to the soap session indicating success,
+			# but note that the queue was already running.
+
+			$res->content({ success => 1, err => "queue $name already running" });
+			$kernel->post(soap => DONE => $res);
+
+			last;
+		};
+
+		$_ == HOPKINS_QUEUE_NOT_FOUND and do
+		{
+			# the specified queue was unable to be found in
+			# the configuration.  post a DONE event to the
+			# soap session; this will cause a SOAP response
+			# to be sent back to the client.
+
+			$res->content({ success => 0, err => "invalid queue $name" });
+			$kernel->post(soap => DONE => $res);
+			last;
+		};
 	}
 }
 
@@ -194,11 +252,10 @@ sub queue_start_waitchk
 
 	Hopkins->log_debug("queue_start_waitchk: checking status of queue $name");
 
-	my $api			= new POE::API::Peek;
-	my @sessions	= map { $kernel->alias($_) } $api->session_list;
-	my $started		= scalar grep { $_ eq "queue.$name" } @sessions;
+	my $queue	= $kernel->call(manager => queue_check => $name);
+	my $running	= $queue ? $queue->is_running : 0;
 
-	if ($started) {
+	if ($running) {
 		# the session was located; the queue is now running.
 		#
 		# post a DONE event to the soap session; this will
@@ -240,36 +297,24 @@ sub queue_stop
 	my $client	= $res->connection->remote_ip;
 	my $params	= $res->soapbody;
 	my ($name)	= map { $params->{$_} } sort keys %$params;
+	my $queue	= $kernel->call(manager => queue_check => $name);
 
 	Hopkins->log_debug("queue_stop request received from $client for $name");
 
-	$kernel->call("queue.$name" => 'stop');
-	$kernel->alarm(queue_stop_waitchk => time + HOPKINS_QUEUE_STATUS_WAIT_TIME, $res, $name, 0);
+	$kernel->post(manager => queue_stop => $name);
+	$kernel->alarm(queue_stop_waitchk => time + HOPKINS_QUEUE_STATUS_WAIT_TIME, $res, $queue, 0);
 }
 
 sub queue_stop_waitchk
 {
 	my $kernel	= $_[KERNEL];
 	my $res		= $_[ARG0];
-	my $name	= $_[ARG1];
+	my $queue	= $_[ARG1];
 	my $iter	= $_[ARG2];
 
-	Hopkins->log_debug("queue_stop_waitchk: checking status of queue $name");
+	Hopkins->log_debug('queue_stop_waitchk: checking status of queue ' . $queue->name);
 
-	my $api			= new POE::API::Peek;
-	my @sessions	= map { $kernel->alias($_) } $api->session_list;
-	my $stopped		= not scalar grep { $_ eq "queue.$name" } @sessions;
-
-	if ($stopped) {
-		# the session is gone; the queue is now stopped.
-		#
-		# post a DONE event to the soap session; this will
-		# cause a SOAP response to be sent back to the
-		# client.
-
-		$res->content({ success => 1 });
-		$kernel->post(soap => DONE => $res);
-	} else {
+	if ($queue->is_running) {
 		# if the session was found, we'll try to wait a bit
 		# for it to be stopped.  if we exceed the maximum
 		# number of wait iterations, we'll return an error
@@ -279,13 +324,22 @@ sub queue_stop_waitchk
 			# exceeded maximum wait iterations; return an
 			# error to the client.
 
-			$res->content({ success => 0, err => "unable to stop $name" });
+			$res->content({ success => 0, err => 'unable to stop ' . $queue->name });
 		} else {
 			# else we'll go another round.  set a kernel
 			# alarm for the appropriate time.
 
-			$kernel->alarm(queue_stop_waitchk => time + HOPKINS_QUEUE_STATUS_WAIT_TIME, $res, $name, ++$iter);
+			$kernel->alarm(queue_stop_waitchk => time + HOPKINS_QUEUE_STATUS_WAIT_TIME, $res, $queue, ++$iter);
 		}
+	} else {
+		# the session is gone; the queue is now stopped.
+		#
+		# post a DONE event to the soap session; this will
+		# cause a SOAP response to be sent back to the
+		# client.
+
+		$res->content({ success => 1 });
+		$kernel->post(soap => DONE => $res);
 	}
 }
 
